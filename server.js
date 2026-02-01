3f8b202a915ae0dbe710546669128fadf7f65298
Wilian96 <64976355+Wilian96@users.noreply.github.com>
2026-01-31 22:56:09 -0300

    Criada a primeira Rota GET, e o servidor.

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+// Sample route
+app.get('/users', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
